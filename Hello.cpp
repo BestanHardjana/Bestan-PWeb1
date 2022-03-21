90189aa29675c30ed5f3484c07d9ad84a4084dcb
HabiePoN3 <bestan.harjana@gmail.com>
2022-03-21 12:04:26 +0700

    Push tugas Pweb 1

---
+++ Hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	cout << "Hello, you want me to greet you?" << endl;
+	cout << "so... Hello, Hello World" << endl;
+	cout << "Hello Everybody :3" << endl;
+	return 0;
+}
